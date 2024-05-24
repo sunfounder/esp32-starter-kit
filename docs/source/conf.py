42b5c999431f5a08dccb96992de5a3e2493354fa
harumiume <127194455+harumiume@users.noreply.github.com>
2024-05-24 18:08:48 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,7 +102,7 @@ rst_epilog = """
 
 .. |link_sf_facebook| raw:: html
 
-    <a href="https://bit.ly/3V2Z4gW" target="_blank">here</a>
+    <a href="https://bit.ly/raphaelkit" target="_blank">here</a>
 
 .. |link_spanish_tutorials| raw:: html
 
