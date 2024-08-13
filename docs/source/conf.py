f57cf525eef849cf04bab8a9a8fd70624f622436
haru$ <1736312423@qq.com>
2024-08-13 10:00:39 +0800

    fix problem

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,7 @@ rst_epilog = """
 
  <a href="https://docs.sunfounder.com/projects/esp32-starter-kit/fr/latest/" target="_blank">Tutoriels en ligne en français</a>
 
- .. |link_it_tutorials| raw:: html
+.. |link_it_tutorials| raw:: html
 
  <a href="https://docs.sunfounder.com/projects/esp32-starter-kit/it/latest/" target="_blank">tutorial online in italiano</a>
 
